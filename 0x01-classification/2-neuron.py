c98295c5075ef138013d360ee55d747c674d8dbb
ABERKOUKS Hafssa <66857045+ABERKOUKS@users.noreply.github.com>
2021-03-31 17:36:15 +0200

    Update 2-neuron.py

--- 0x01-classification/2-neuron.py
+++ 0x01-classification/2-neuron.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 class Neuron(object):
-    def__init__(self, nx):
+    def __init__(self, nx):
         if not type(nx) is int:
   	    raise TypeError("nx must be an integer")
   	if nx < 1:
