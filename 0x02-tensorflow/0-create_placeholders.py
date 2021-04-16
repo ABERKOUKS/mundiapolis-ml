66f70fdc477aa062e69a5554077bbfde794b06e2
ABERKOUKS Hafssa <66857045+ABERKOUKS@users.noreply.github.com>
2021-04-16 10:45:43 +0000

    Update 0-create_placeholders.py

--- 0x02-tensorflow/0-create_placeholders.py
+++ 0x02-tensorflow/0-create_placeholders.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 def create_placeholders(nx, classes):
 	x=tf.placeholder( shape = (None,nx), dtype = tf.float32)
 	y=tf.placeholder( shape = (None,classes), dtype = tf.float32)
