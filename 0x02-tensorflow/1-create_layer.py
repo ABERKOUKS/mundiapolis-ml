2cc4a09d22a54cd71f8c0aeb33344bc091fbfb00
ABERKOUKS Hafssa <66857045+ABERKOUKS@users.noreply.github.com>
2021-04-18 14:22:08 +0000

    Update 1-create_layer.py

--- 0x02-tensorflow/1-create_layer.py
+++ 0x02-tensorflow/1-create_layer.py
@@ -2,6 +2,5 @@
 import tensorflow as tf
 def create_layer(prev, n, activation):
 	init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-	nn= tf.layers.dense( units=n,kernel_initializer= init, activation=activation , name='layer')
-	tanh= nn(prev)
+	tanh = tf.layers.dense(prev, n,kernel_initializer= init, activation=activation , name='layer')
 	return tanh
