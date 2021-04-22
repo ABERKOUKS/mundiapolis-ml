9a97b9953cf7a8aa42caf353f3a7f135fd3c0911
ABERKOUKS Hafssa <66857045+ABERKOUKS@users.noreply.github.com>
2021-04-22 23:49:52 +0000

    Update 2-forward_prop.py

--- 0x02-tensorflow/2-forward_prop.py
+++ 0x02-tensorflow/2-forward_prop.py
@@ -8,5 +8,8 @@ create_layer = __import__('1-create_layer').create_layer
 def forward_prop(x, layer_sizes=[], activations=[]):
 
     for i in range(len(layer_sizes)):
-        prediction= create_layer(prediction, layer_sizes[i], activations[i])
+        if i == 0:
+            prediction = create_layer(x, layer_sizes[0], activations[0])
+        else:
+            prediction = create_layer(prediction, layer_sizes[i], activations[i])
     return prediction
