aa903fe40c181f62721c1b888fd32365403d8f52
ABERKOUKS Hafssa <66857045+ABERKOUKS@users.noreply.github.com>
2021-05-21 09:27:25 +0100

    Update 1-input.py

--- 0x06-keras/1-input.py
+++ 0x06-keras/1-input.py
@@ -1,23 +1,10 @@
 #!/usr/bin/env python3
-"""Script for build a DNN in keras"""
 
 import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    """
-    Function to create a DNN using keras
-    Args:
-        nx: number of input features to the network
-        layers: list containing the number of nodes in each
-                layer of the network
-        activations: list containing the activation functions
-                     used for each layer of the network
-        lambtha: L2 regularization parameter
-        keep_prob: probability that a node will be kept for dropout
-    Returns: Keras model
-
-    """
+   
 
     inputs = K.Input(shape=(nx,))
     L2 = K.regularizers.l2(lambtha)
